d1a8781180ab99bdf87a161b88fcc4115509eabb
jorgeecr <jorgeecr@gmail.com>
2020-05-14 22:18:52 -0500

    Create Classifier.java

---
+++ app/src/main/tflite/Classifier.java
@@ -0,0 +1,199 @@
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+import android.os.SystemClock;
+import android.os.Trace;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.examples.classification.env.Logger;
+import org.tensorflow.lite.examples.classification.tflite.Classifier.Device;
+import org.tensorflow.lite.gpu.GpuDelegate;
+import org.tensorflow.lite.nnapi.NnApiDelegate;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeOp.ResizeMethod;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+/** A classifier specialized to label images using TensorFlow Lite. */
+public abstract class Classifier {
+
+  private static final String TAG = "Classifier";
+
+
+  /** The model type used for classification. */
+  public enum Model {
+    GRID_DETECTION,
+    OTHER
+  }
+
+  /** The runtime device type used for executing classification. */
+  public enum Device {
+    CPU,
+    NNAPI,
+    GPU
+  }
+
+  /** The loaded TensorFlow Lite model. */
+  private MappedByteBuffer tfliteModel;
+
+  /** Image size along the x axis. */
+  private final int imageSizeX;
+
+  /** Image size along the y axis. */
+  private final int imageSizeY;
+
+  /** Optional GPU delegate for accleration. */
+  private GpuDelegate gpuDelegate = null;
+
+  /** Optional NNAPI delegate for accleration. */
+  private NnApiDelegate nnApiDelegate = null;
+
+  /** An instance of the driver class to run model inference with Tensorflow Lite. */
+  protected Interpreter tflite;
+
+  /** Options for configuring the Interpreter. */
+  private final Interpreter.Options tfliteOptions = new Interpreter.Options();
+
+  /** Input image TensorBuffer. */
+  private TensorImage inputImageBuffer;
+
+  /** Output probability TensorBuffer. */
+  private final TensorBuffer outputProbabilityBuffer;
+
+  /** Processer to apply post processing of the output probability. */
+  private final TensorProcessor probabilityProcessor;
+
+  /**
+   * Creates a classifier with the provided configuration.
+   *
+   * @param activity The current Activity.
+   * @param model The model to use for classification.
+   * @param device The device to use for classification.
+   * @param numThreads The number of threads to use for classification.
+   * @return A classifier with the desired configuration.
+   */
+
+
+  /** Initializes a {@code Classifier}. */
+  protected Classifier(Activity activity, Device device, int numThreads) throws IOException {
+    tfliteModel = FileUtil.loadMappedFile(activity, getModelPath());
+    switch (device) {
+      case NNAPI:
+        nnApiDelegate = new NnApiDelegate();
+        tfliteOptions.addDelegate(nnApiDelegate);
+        break;
+      case GPU:
+        gpuDelegate = new GpuDelegate();
+        tfliteOptions.addDelegate(gpuDelegate);
+        break;
+      case CPU:
+        break;
+    }
+    tfliteOptions.setNumThreads(numThreads);
+    tflite = new Interpreter(tfliteModel, tfliteOptions);
+
+    // Reads type and shape of input and output tensors, respectively.
+    int imageTensorIndex = 0;
+    int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+    imageSizeY = imageShape[1];
+    imageSizeX = imageShape[2];
+    DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+    int probabilityTensorIndex = 0;
+    int[] probabilityShape =
+        tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+    DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+
+    // Creates the input tensor.
+    inputImageBuffer = new TensorImage(imageDataType);
+
+    // Creates the output tensor and its processor.
+    outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+
+    // Creates the post processor for the output probability.
+    probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+
+    Log.d(TAG, "Created a Tensorflow Lite Image Classifier.");
+  }
+
+  /** Runs inference and returns the classification results. */
+  public int recognizeImage(final Bitmap bitmap, int sensorOrientation) {
+    // Logs this method so that it can be analyzed with systrace.
+    Trace.beginSection("recognizeImage");
+
+    Trace.beginSection("loadImage");
+    long startTimeForLoadImage = SystemClock.uptimeMillis();
+    inputImageBuffer = loadImage(bitmap);
+    long endTimeForLoadImage = SystemClock.uptimeMillis();
+    Trace.endSection();
+    Log.v(TAG, "Timecost to load the image: " + (endTimeForLoadImage - startTimeForLoadImage));
+
+    // Runs the inference call.
+    Trace.beginSection("runInference");
+    long startTimeForReference = SystemClock.uptimeMillis();
+    tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
+    long endTimeForReference = SystemClock.uptimeMillis();
+    Trace.endSection();
+    Log.v(TAG, "Timecost to run model inference: " + (endTimeForReference - startTimeForReference));
+
+    // ejecutar siguiente modelo
+    if (outputProbabilityBuffer[0][0] > 30) {
+      return 0; 
+    }
+      //aplicar siguiente modelo 
+    
+    Trace.endSection();
+  }
+
+  /** Closes the interpreter and model to release resources. */
+  public void close() {
+    if (tflite != null) {
+      tflite.close();
+      tflite = null;
+    }
+    if (gpuDelegate != null) {
+      gpuDelegate.close();
+      gpuDelegate = null;
+    }
+    if (nnApiDelegate != null) {
+      nnApiDelegate.close();
+      nnApiDelegate = null;
+    }
+    tfliteModel = null;
+  }
+
+  /** Get the image size along the x axis. */
+  public int getImageSizeX() {
+    return imageSizeX;
+  }
+
+  /** Get the image size along the y axis. */
+  public int getImageSizeY() {
+    return imageSizeY;
+  }
+
+  /** Loads input image, and applies preprocessing. */
+  private TensorImage loadImage(final Bitmap bitmap) {
+    // Loads bitmap into a TensorImage.
+    inputImageBuffer.load(bitmap);
+    // TODO(b/143564309): Fuse ops inside ImageProcessor.
+    ImageProcessor imageProcessor =
+        new ImageProcessor.Builder()
+            .add(new ResizeOp(imageSizeX, imageSizeY, ResizeMethod.NEAREST_NEIGHBOR))
+            .build();
+    return imageProcessor.process(inputImageBuffer);
+  }
+
+
+  /** Gets the name of the model file stored in Assets. */
+  protected abstract String getModelPath();
